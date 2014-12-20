e16448e37e40e24bb5d9c204f355fc230370e410
Greg Caporaso <gregcaporaso@gmail.com>
2014-12-19 18:29:08 -0700

    MAINT: removed some comments

--- qiime/plugins/qiime/preprocessing/methods.py
+++ qiime/plugins/qiime/preprocessing/methods.py
@@ -48,39 +48,28 @@ def demux_sequences(sequences, barcodes, barcode_map):
             result.failures.add(sequence)
             result.bad_barcode_count += 1
     return result
-#
-#
-# @method.parameter('barcode', RawSequence, help='')
-# @method.parameter('barcode_map', SampleMetadata, help='')
-# @method.raises(KeyError, "barcode is not in barcode_map")
-# def _dumux_sequence(barcode, barcode_map):
-#     """Demultiplex sequence
-#     """
-#     return barcode_map[barcode]
-#
-#
-#
-#     @method.parameter('max_bad_run_length', Range(Integer, 0, None),
-#     help='max number of consecutive low quality base calls '
-#     'allowed before truncating a read', default=3)
-#     @method.parameter('min_per_read_length_fraction', Range(Float, 0.0, 1.0),
-#     help='min number of consecutive high quality base calls to '
-#     'include a read (per single end read) as a fraction of '
-#     'the input read length', default=0.75)
-#     @method.parameter('sequence_max_n', Range(Integer, 0, None),
-#     help='maximum number of N characters allowed in a sequence '
-#     'to retain it -- this is applied after quality '
-#     'trimming, and is total over combined paired end reads '
-#     'if applicable', default=0)
-#     @method.parameter('start_seq_id', Range(Integer, 0, None),
-#     help='start seq_ids as ascending integers beginning with '
-#     'start_seq_id', default=0)
-#
-#
-#
-# @method.register("Quality filter sequences")
-# @method.parameter('sequences', RawSequences, help='')
-# @method.parameter('barcodes', RawSequences, help='')
-# @method.result('demultiplexed_sequences', QualityFilteredSequences, help='')
-# def quality_filter_sequences(sequences, barcodes):
-#     return BiologicalSequence('')
+
+@qiime.method
+@method.parameter('sequences', DemultiplexedSequences, help='')
+@method.parameter('max_bad_run_length', Range(Integer, 0, None),
+    help='max number of consecutive low quality base calls '
+    'allowed before truncating a read', default=3)
+@method.parameter('min_per_read_length_fraction', Range(Float, 0.0, 1.0),
+    help='min number of consecutive high quality base calls to '
+    'include a read (per single end read) as a fraction of '
+    'the input read length', default=0.75)
+@method.parameter('sequence_max_n', Range(Integer, 0, None),
+    help='maximum number of N characters allowed in a sequence '
+    'to retain it -- this is applied after quality '
+    'trimming, and is total over combined paired end reads '
+    'if applicable', default=0)
+@method.parameter('start_seq_id', Range(Integer, 0, None),
+    help='start seq_ids as ascending integers beginning with '
+    'start_seq_id', default=0)
+@method.parameter('phred_quality_threshold', Range(Integer, 0, None),
+    help='the maximum unacceptable Phred quality score (e.g., '
+    'for Q20 and better, specify -q 19)', default=3)
+@method.result('result', QualityFilterResult, help='')
+def quality_filter_sequences(sequences, ...):
+    result = QualityFilterResult()
+    return result
